c74658f695d152ff4bf3dc1c5d03a926a44a84b8
Aleksey <nirall@mail.ru>
2021-02-06 15:16:18 +0500

    update

---
+++ docs/_next/static/dLU5vucSaWVuYBNISnO8L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-60b9b40ce0e60a44d809.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-868de72ec1fd4c5aa46f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
