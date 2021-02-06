e8af2d6b7235109f13716c45b5f97db4187a043a
Aleksey <nirall@mail.ru>
2021-02-06 15:27:30 +0500

    uodate

---
+++ docs/_next/static/ijnsG0Y_Go61h0fBvOjz5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-eb25bfadb6bc4aaaf868.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-868de72ec1fd4c5aa46f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
